82ad7d666e742c23f0a89d3470802f38a12fd348
emewarp <martawarp@gmail.com>
2020-07-09 19:42:06 +0200

    exceptions control

--- MyCalendar/MyCalendarApi/Controllers/CalendarEventsController.cs
+++ MyCalendar/MyCalendarApi/Controllers/CalendarEventsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MyCalendarContracts;
 using MyCalendarContracts.Contracts;
 using MyCalendarModels.Models;
+using MyCalendarContracts;
+using System;
+using System.Net;
 
 namespace MyCalendarApi.Controllers
 {
@@ -17,33 +19,75 @@ namespace MyCalendarApi.Controllers
         }
 
         [HttpGet("{calendarId}/events")]
-        public CalendarEvents GetAllEvents(string calendarId)
+        public IActionResult GetAllEvents(string calendarId)
         {
-            return service.GetAllEvents(calendarId);
+            try
+            {
+                CalendarEvents events = service.GetAllEvents(calendarId);
+                return events != null ? (IActionResult)Ok(events) : (IActionResult)BadRequest();
+            }
+            catch (Google.GoogleApiException e)
+            {
+                return e.HttpStatusCode.Equals(HttpStatusCode.NotFound) ? (IActionResult)NotFound(e.Message) : e.HttpStatusCode.Equals(HttpStatusCode.InternalServerError) ? StatusCode(500, e.Message) : (IActionResult)BadRequest(e.Message);
+            }
+
         }
 
         [HttpGet("{calendarId}/events/{eventId}")]
-        public CalendarEvent GetEvent(string calendarId, string eventId)
+        public IActionResult GetEvent(string calendarId, string eventId)
         {
-            return service.GetEvent(calendarId, eventId);
+            try
+            {
+                CalendarEvent calendarEvent = service.GetEvent(calendarId, eventId);
+                return calendarEvent != null ? (IActionResult)Ok(calendarEvent) : (IActionResult)BadRequest();
+            }
+            catch (Google.GoogleApiException e)
+            {
+                return e.HttpStatusCode.Equals(HttpStatusCode.NotFound) ? (IActionResult)NotFound() : (IActionResult)BadRequest(e.Message);
+            }
+
         }
 
         [HttpPost("{calendarId}/events")]
-        public CalendarEvent CreateEvent(CalendarEvent myEvent, string calendarId)
+        public IActionResult CreateEvent(CalendarEvent myEvent, string calendarId)
         {
-            return service.CreateEvent(myEvent, calendarId);
+            try
+            {
+                CalendarEvent newEvent = service.CreateEvent(myEvent, calendarId);
+                return newEvent != null ? StatusCode(201, newEvent) : (IActionResult)BadRequest();
+            }
+            catch (Google.GoogleApiException e)
+            {
+                Console.WriteLine(e);
+                return e.HttpStatusCode.Equals(HttpStatusCode.Conflict) ? (IActionResult)Conflict(e.Message) : (IActionResult)BadRequest(e.Message);
+            }
         }
 
         [HttpPut("{calendarId}/events/{eventId}")]
-        public CalendarEvent UpdateEvent(CalendarEvent update, string calendarId, string eventId)
+        public IActionResult UpdateEvent(CalendarEvent update, string calendarId, string eventId)
         {
-            return service.UpdateEvent(update, calendarId, eventId);
+            try
+            {
+                CalendarEvent updatedEvent = service.UpdateEvent(update, calendarId, eventId);
+                return updatedEvent != null ? (IActionResult)Ok(updatedEvent) : (IActionResult)BadRequest();
+            }
+            catch (Google.GoogleApiException e)
+            {
+                return e.HttpStatusCode.Equals(HttpStatusCode.NotFound) ? (IActionResult)NotFound(e.Message) : (IActionResult)BadRequest(e.Message);
+            }
         }
 
         [HttpDelete("{calendarId}/events/{eventId}")]
-        public bool DeleteEvent(string calendarId, string eventId)
+        public IActionResult DeleteEvent(string calendarId, string eventId)
         {
-            return service.DeleteEvent(calendarId, eventId);
+            try
+            {
+                return service.DeleteEvent(calendarId, eventId) ? (IActionResult)Ok(eventId) : (IActionResult)BadRequest();
+            }
+            catch (Google.GoogleApiException e)
+            {
+                return e.HttpStatusCode.Equals(HttpStatusCode.NotFound) ? (IActionResult)NotFound(e.Message) : (IActionResult)BadRequest(e.Message);
+            }
         }
     }
 }
